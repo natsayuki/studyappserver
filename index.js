e11b145557a8c0971ec1458a103018715dcbc78c
the42ndturtle <the42ndturtle@gmail.com>
2018-09-14 19:01:57 -0400

    added terms

--- index.js
+++ index.js
@@ -7,7 +7,19 @@ const server = http.Server(app);
 let sets = {'11111111': {
   title: 'test app',
   terms: {
-    'test': 'app'
+    'term1': 'answer1',
+    'term2': 'answer2',
+    'term3': 'answer3',
+    'term4': 'answer4',
+    'term5': 'answer5',
+    'term6': 'answer6',
+    'term7': 'answer7',
+    'term8': 'answer8',
+    'term9': 'answer9',
+    'term10': 'answer10',
+    'term11': 'answer11',
+    'term12': 'answer12',
+    'term13': 'answer13',
   }
 }}
 
